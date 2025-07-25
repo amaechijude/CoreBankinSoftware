6bb1f1822ac026d3cd312888060576b24f942bc2
amaechijude <ugwuamaechi5@gmail.com>
2025-07-25 17:20:19 +0100

    twitter

--- CustomerProfile/src/Shared/Global/GlobalConstansts.cs
+++ CustomerProfile/src/Shared/Global/GlobalConstansts.cs
@@ -8,12 +8,12 @@ namespace src.Shared.Global
         public static string GenerateVerificationCode()
         {
             using var rng = RandomNumberGenerator.Create();
-            byte[] randomNumber = new byte[4]; // 4 bytes will give us a number up to 2^32 - 1
+            byte[] randomNumber = new byte[4]; 
             rng.GetBytes(randomNumber);
 
             // Use Math.Abs to handle negative numbers, then modulo to get 7 digits
             int code = Math.Abs(BitConverter.ToInt32(randomNumber, 0)) % 10_000_000;
-            return code.ToString("D7"); // Format as a 7-digit string with leading zeros
+            return code.ToString("D7");
         }
     }
 }
